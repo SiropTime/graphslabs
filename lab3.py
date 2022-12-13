86895a8d8cfbd655c410795489b10d2e7cd548f9
SiropTime <sirop4beats@gmail.com>
2022-12-14 00:21:44 +0300

    3 lab

--- lab3.py
+++ lab3.py
@@ -5,9 +5,11 @@ from random import randint
 
 if __name__ == '__main__':
     n = int(input("Введите размерность матрицы: "))
-    row = np.array([randint(0, 100) for i in range(n)])
-    col = np.array([randint(0, 100) for j in range(n)])
-    data = np.array([randint(0, 100) for k in range(n)])
-    sparse_matrix: scipy.sparse.csc_matrix = scipy.sparse.csc_matrix((data, (row, col)), (n, n)).toarray()
-
+    row = np.array([randint(1, n-1) for i in range(n-1)])
+    col = np.array([randint(1, n-1) for j in range(n-1)])
+    data = np.array([randint(1, 1000) for k in range(n-1)])
+    sparse_matrix: scipy.sparse.csc_matrix = scipy.sparse.csc_matrix((data, (row, col)), shape=(n, n)).toarray()
     print(sparse_matrix)
+    pre = [sparse_matrix[i,j] if (i + j) % 2 == 0 else 0 for i, j in zip(*sparse_matrix.nonzero())]
+    res = sum(pre)
+    print(res)
